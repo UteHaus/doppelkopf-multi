d233afa23016aad2a89d914e1cf8c3b48e841348
Florian v <j.v@posteo.de>
2021-01-22 20:58:24 +0100

    upgrade angular to 11

--- client/src/doppelkopf/game-table/player-view/announcement-selector/announcement-selector.component.spec.ts
+++ client/src/doppelkopf/game-table/player-view/announcement-selector/announcement-selector.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AnnouncementSelectorComponent } from './announcement-selector.component';
 
@@ -6,7 +6,7 @@ describe('AnnouncementSelectorComponent', () => {
   let component: AnnouncementSelectorComponent;
   let fixture: ComponentFixture<AnnouncementSelectorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AnnouncementSelectorComponent ]
     })
--- client/src/doppelkopf/game-table/players-table/players-table.component.spec.ts
+++ client/src/doppelkopf/game-table/players-table/players-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlayersTableComponent } from './players-table.component';
 
@@ -6,7 +6,7 @@ describe('PlayersTableComponent', () => {
   let component: PlayersTableComponent;
   let fixture: ComponentFixture<PlayersTableComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlayersTableComponent ]
     })
